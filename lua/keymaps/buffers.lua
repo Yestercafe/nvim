c4ddd839e4faa08bc3046091c3775fa1a19e8714
Ivan Chien <qyc027@gmail.com>
2023-03-14 17:31:22 +0800

    Update keymaps of bwipeout and tabclose

--- lua/keymaps/buffers.lua
+++ lua/keymaps/buffers.lua
@@ -4,10 +4,11 @@ vim.keymap.set('n', '<leader>bH', '<CMD>bf<CR>')
 vim.keymap.set('n', '<leader>bL', '<CMD>bl<CR>')
 -- vim.keymap.set('n', '<leader>bb', '<CMD>b ')
 vim.keymap.set('n', '<leader>B', '<CMD>buffers<CR>')
-vim.keymap.set('n', '<leader>bk', '<CMD>bw<CR>')
+vim.keymap.set('n', '<leader>bx', '<CMD>bw<CR>')
 vim.keymap.set('n', '<leader>br', '<CMD>brewind<CR>')
 
 vim.keymap.set('n', '<leader>jn', '<CMD>tabe<CR>')
 vim.keymap.set('n', '<leader>jl', '<CMD>tabnext<CR>')
 vim.keymap.set('n', '<leader>jh', '<CMD>tabprev<CR>')
+vim.keymap.set('n', '<leader>jx', '<CMD>tabclose<CR>')
 
