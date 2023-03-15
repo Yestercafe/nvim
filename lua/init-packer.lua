162dd6a5e3fb9a9a90f253d5dd7c24cfb499e841
Ivan Chien <qyc027@gmail.com>
2023-03-15 13:02:13 +0800

    Add Markdown preview support

--- lua/init-packer.lua
+++ lua/init-packer.lua
@@ -91,5 +91,12 @@ return require('packer').startup(function(use)
 
     use 'wakatime/vim-wakatime'
 
+    -- Markdown
+    use {
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    }
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 end)
 
