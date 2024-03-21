be7e0d4686cc7827a9bcb1dbaf20ea65d042d52e
Ivan Chien <qyc027@gmail.com>
2024-03-22 00:25:11 +0800

    feat(LazyVim): customize

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+-- ESC
+map("i", "kj", "<ESC>")
+
+-- Alt
+map("i", "<esc><bs>", "<C-w>")
+
+-- Clear search
+map("n", "<leader><cr>", "<cmd>nohl<cr>", { desc = "Clear search" })
+
+-- Move to window
+unmap("n", "<C-h>")
+unmap("n", "<C-j>")
+unmap("n", "<C-k>")
+unmap("n", "<C-l>")
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>", { desc = "Go to left window", remap = true })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>", { desc = "Go to lower window", remap = true })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>", { desc = "Go to upper window", remap = true })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>", { desc = "Go to right window", remap = true })
+
+-- Save file
+map("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save file" })
---
+++ lua/plugins/additions.lua
@@ -0,0 +1,22 @@
+return {
+  { import = "lazyvim.plugins.extras.vscode" },
+
+  { import = "lazyvim.plugins.extras.ui.edgy" },
+  { import = "lazyvim.plugins.extras.ui.mini-starter" },
+
+  { import = "lazyvim.plugins.extras.coding.yanky" },
+
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.haskell" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.dap.core" },
+
+  { import = "lazyvim.plugins.extras.editor.aerial" },
+  { import = "lazyvim.plugins.extras.editor.leap" },
+  { import = "lazyvim.plugins.extras.editor.harpoon2" },
+  { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.editor.outline" },
+}
