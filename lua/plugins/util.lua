874119c4051412b13e40ee753ec1de7029de9cdb
Ivan Chien <qyc027@gmail.com>
2024-03-28 21:52:05 +0800

    feat(LazyVim): use echasnovski/mini.comment

--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,4 +1,11 @@
 return {
   { "christoomey/vim-tmux-navigator" },
   { "wakatime/vim-wakatime" },
+  { "echasnovski/mini.comment", enabled = false },
+  {
+    "numToStr/Comment.nvim",
+    init = function()
+      require("Comment").setup()
+    end,
+  },
 }
