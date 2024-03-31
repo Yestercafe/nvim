a22186fa4aa70e2febf268239d793e1bbafb0df9
Ivan Chien <qyc027@gmail.com>
2024-03-31 11:37:45 +0800

    feat(LazyVim): rearrange extra plugins

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,4 +1,11 @@
 return {
+  { "ellisonleao/gruvbox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
   { "christoomey/vim-tmux-navigator" },
   { "wakatime/vim-wakatime" },
   { "echasnovski/mini.comment", enabled = false },
