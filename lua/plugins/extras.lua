9186161ae747cd086639c0cae5acfeecc0c46b0a
Ivan Chien <qyc027@gmail.com>
2024-03-31 11:59:03 +0800

    feat(extras): add nvim-spider

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -8,4 +8,27 @@ return {
       require("Comment").setup()
     end,
   },
+  {
+    "chrisgrieser/nvim-spider",
+    keys = {
+      {
+        "<leader>e",
+        "<cmd>lua require('spider').motion('e')<CR>",
+        mode = { "n", "o", "x" },
+        desc = "spider-e",
+      },
+      {
+        "<leader>w",
+        "<cmd>lua require('spider').motion('w')<CR>",
+        mode = { "n", "o", "x" },
+        desc = "spider-w",
+      },
+      {
+        "<leader>b",
+        "<cmd>lua require('spider').motion('b')<CR>",
+        mode = { "n", "o", "x" },
+        desc = "spider-b",
+      },
+    },
+  },
 }
