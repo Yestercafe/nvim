25ee76d12333ad094debbadf73b0a3fbbfca4df6
Ivan Chien <qyc027@gmail.com>
2024-03-31 11:51:30 +0800

    feat(LazyVim): use supertab recipe

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,14 +1,7 @@
 return {
   { "ellisonleao/gruvbox.nvim" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
-    },
-  },
   { "christoomey/vim-tmux-navigator" },
   { "wakatime/vim-wakatime" },
-  { "echasnovski/mini.comment", enabled = false },
   {
     "numToStr/Comment.nvim",
     init = function()
