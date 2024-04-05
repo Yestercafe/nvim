cf70071a5d1161be42e7cc6f33ab28507ad82ce2
Ivan Chien <qyc027@gmail.com>
2024-04-05 16:40:34 +0800

    feat(tweak): make bg transparent

--- lua/plugins/tweak.lua
+++ lua/plugins/tweak.lua
@@ -1,8 +1,18 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      -- colorscheme = "catppuccin",
     },
   },
   { "echasnovski/mini.comment", enabled = false },
