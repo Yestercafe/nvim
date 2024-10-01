bf80d7ca9916ea7b9799223b01b75a7ed19f63d9
Ivan Chien <qyc027@gmail.com>
2024-10-01 20:07:04 +0800

    feat(NeoTree): auto use NeoTree when start directory

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -25,4 +25,21 @@ return {
       },
     },
   },
+  init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
+      desc = 'Start Neo-tree with directory',
+      once = true,
+      callback = function()
+        if package.loaded['neo-tree'] then
+          return
+        else
+          local stats = vim.uv.fs_stat(vim.fn.argv(0))
+          if stats and stats.type == 'directory' then
+            require 'neo-tree'
+          end
+        end
+      end,
+    })
+  end,
 }
