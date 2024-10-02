97742ea025b4dc186aa0205e335f8278197e490d
Ivan Chien <qyc027@gmail.com>
2024-10-02 13:37:07 +0800

    feat(ks): enhance keymaps

---
+++ lua/keymap.lua
@@ -0,0 +1,113 @@
+local map = vim.keymap.set
+local unmap = vim.keymap.del
+
+--  See `:help vim.keymap.set()`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+-- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Save current buffer
+vim.keymap.set('n', '<leader>fs', ':w<CR>', { desc = 'Save buffer', silent = true })
+
+-- Quit buffer
+vim.keymap.set('n', '<leader>qq', ':q<CR>', { desc = 'Quit buffer', silent = true })
+
+-- Insert mode: Alt-backspace means C-w
+vim.keymap.set('i', '<Esc><BS>', '<C-w>')
+
+-- Window management
+vim.keymap.set('n', '<C-w>2', '<C-w>s', { desc = 'Split window' })
+vim.keymap.set('n', '<C-w>_', '<C-w>s', { desc = 'Split window' })
+vim.keymap.set('n', '<C-w>3', '<C-w>v', { desc = 'Split window vertically)' })
+vim.keymap.set('n', '<C-w>|', '<C-w>v', { desc = 'Split window vertically' })
+
+-- UI
+vim.keymap.set('n', '<leader>uf', function()
+  options.auto_format = not options.auto_format
+  vim.notify(string.format('Auto format set to `%s`', tostring(options.auto_format)), 'info')
+end, { desc = 'Toggle auto format', silent = true })
+
+-- Terminal
+local lazy_terminal = require 'lazy.components.terminal'
+lazy_terminal.setup()
+vim.keymap.set('n', '<leader>ft', function()
+  lazy_terminal.open()
+end, { desc = 'Terminal' })
+vim.keymap.set('n', '<leader>tt', function()
+  lazy_terminal.open()
+end, { desc = 'Terminal' })
+
+-- Better up and down
+map({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+map({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+map({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+
+-- Resize window using <ctrl> arrow keys
+map('n', '<C-Up>', '<cmd>resize +2<cr>', { desc = 'Increase Window Height' })
+map('n', '<C-Down>', '<cmd>resize -2<cr>', { desc = 'Decrease Window Height' })
+map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease Window Width' })
+map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase Window Width' })
+
+-- Move Lines
+map('n', '<A-j>', '<cmd>m .+1<cr>==', { desc = 'Move Down' })
+map('n', '<A-k>', '<cmd>m .-2<cr>==', { desc = 'Move Up' })
+map('i', '<A-j>', '<esc><cmd>m .+1<cr>==gi', { desc = 'Move Down' })
+map('i', '<A-k>', '<esc><cmd>m .-2<cr>==gi', { desc = 'Move Up' })
+map('v', '<A-j>', ":m '>+1<cr>gv=gv", { desc = 'Move Down' })
+map('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
+
+-- Clear search, diff update and redraw
+-- taken from runtime/lua/_editor.lua
+map('n', '<leader>ur', '<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>', { desc = 'Redraw / Clear hlsearch / Diff Update' })
+
+-- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+map('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
+map('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+map('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
+map('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+map('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+
+-- Add undo break-points
+map('i', ',', ',<c-g>u')
+map('i', '.', '.<c-g>u')
+map('i', ';', ';<c-g>u')
+
+-- Better indenting
+map('v', '<', '<gv')
+map('v', '>', '>gv')
+
+-- New file
+map('n', '<leader>fn', '<cmd>enew<cr>', { desc = 'New File' })
+
+-- Home and end
+map({ 'n', 'v' }, '<S-h>', '^')
+map({ 'n', 'v' }, '<S-l>', '$')
