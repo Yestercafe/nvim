8699d2790612dd9b41dfe61e21627815829b0e82
Ivan Chien <qyc027@outlook.com>
2025-01-25 17:21:21 +0800

    feat(LazyVim): configurable GitHub base URL

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,8 +1,10 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local github_baseurl = "httsp://github.com/"
+
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", github_baseurl .. "folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -43,4 +45,7 @@ require("lazy").setup({
       },
     },
   },
+    git = {
+        url_format = github_baseurl .. '%s.git'
+    },
 })
