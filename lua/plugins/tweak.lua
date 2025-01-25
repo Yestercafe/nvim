9a6f4e0fa7b799389d653d0f82316fb5fcd3a7c1
Ivan Chien <qyc027@outlook.com>
2025-01-25 17:05:46 +0800

    feat: use `tokyonight` as default colorscheme

--- lua/plugins/tweak.lua
+++ lua/plugins/tweak.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "tokyonight",
     },
   },
   { "echasnovski/mini.comment", enabled = false },
